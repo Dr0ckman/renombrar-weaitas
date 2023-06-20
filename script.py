b799603948dbaa8a8158f1deb67256cea82a20a0
Dr0ckman <cmn.kkckdbb@gmail.com>
2023-06-20 13:36:04 -0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,17 @@
+import os
+
+filenames = []
+counter = 1
+
+for filename in os.listdir(os.getcwd()):
+  file_extension = os.path.splitext(filename)[1]
+  print(file_extension)
+
+  if len(filename) > 10:
+    new_filename = filename[:9] + "_" + str(counter) + file_extension
+  else:
+    new_filename = filename + "_" + str(counter) + file_extension
+
+  if new_filename not in os.listdir(os.getcwd()) and filename != "script.py":
+    os.rename(filename, new_filename)
+  counter += 1
